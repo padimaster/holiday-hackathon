437e88d71a04cb894e6eee21904fed525cb5d1cd
Alex Padilla <alex.padilla@epn.edu.ec>
2024-12-31 18:49:08 -0500

    Update dependencies

--- packages/contracts/deploy/utils.ts
+++ packages/contracts/deploy/utils.ts
@@ -1,17 +1,19 @@
-import * as hre from "hardhat";
 import dotenv from "dotenv";
 import { chains } from "@lens-network/sdk/viem";
-import { createPublicClient, createWalletClient, http, Hex } from "viem";
+import { createPublicClient, createWalletClient, http, Hex, Chain } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { eip712WalletActions } from "viem/zksync";
 
 dotenv.config();
 
-export const getPublicClient = () =>
-  createPublicClient({
+export const getPublicClient = () => {
+  console.log(chains.testnet);
+
+  return createPublicClient({
     chain: chains.testnet,
     transport: http(),
   });
+};
 
 export const getAccount = () =>
   privateKeyToAccount(`0x${process.env.PRIVATE_KEY}` as Hex);
